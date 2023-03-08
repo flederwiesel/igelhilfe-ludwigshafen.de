3b0290e7e3a1faf6cb549506f16cfaee171a1184
flederwiesel <flederwiesel@fra-flugplan.de>
2023-03-08 21:49:48 +0100

    Add favicons.

--- index.php
+++ index.php
@@ -51,6 +51,19 @@ default:
 	<meta name="theme-color" content="#066">
 	<title>Igelhilfe Ludwigshafen</title>
 	<link rel="canonical" href="https://igelhilfe-ludwigshafen.de/">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<!-- third-generation iPad with high-resolution Retina display: -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicon144.png" />
+	<!-- iPhone with high-resolution Retina display: -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicon114.png" />
+	<!-- first- and second-generation iPad: -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicon72.png" />
+	<!-- non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+	<link rel="apple-touch-icon-precomposed" href="favicon57.png" />
+	<!-- basic favicon -->
+	<link rel="icon" href="favicon32.png" />
+	<!-- -->
+	<link rel="shortcut icon" href="favicon.ico" />
 	<link rel="stylesheet" href="index.css">
 </head>
 <body>
